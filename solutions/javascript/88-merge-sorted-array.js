8f1814b6dea35a62a445c0ae70d44263c2839c3f
Khang Nguyen <ngkhang0220@gmail.com>
2025-10-19 21:57:22 +0700

    add: 88 - Merge Sorted Array

---
+++ solutions/javascript/88-merge-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    const num1 = nums1[p1];
+    const num2 = nums2[p2];
+
+    if (num1 >= num2) {
+      nums1[p] = num1;
+      p1--;
+    } else {
+      nums1[p] = num2;
+      p2--;
+    }
+    p--;
+  }
+
+  while (p2 >= 0) {
+    nums1[p] = nums2[p2];
+    p2--;
+    p--;
+  }
+};
---
+++ solutions/typescript/88-merge-sorted-array.ts
@@ -0,0 +1,33 @@
+/**
+ * Do not return anything, modify nums1 in-place instead.
+ */
+export function merge(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    const num1 = nums1[p1];
+    const num2 = nums2[p2];
+
+    if (num1 >= num2) {
+      nums1[p] = num1;
+      p1--;
+    } else {
+      nums1[p] = num2;
+      p2--;
+    }
+    p--;
+  }
+
+  while (p2 >= 0) {
+    nums1[p] = nums2[p2];
+    p2--;
+    p--;
+  }
+}
