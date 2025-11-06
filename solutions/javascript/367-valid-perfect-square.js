cd562c4ba4cbe4dee05d8e9e6b950130dbcb5b8d
Khang Nguyen <ngkhang0220@gmail.com>
2025-11-06 13:29:47 +0700

    add: 367 - Valid Perfect Square

---
+++ solutions/javascript/367-valid-perfect-square.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ *
+ * @link https://leetcode.com/problems/valid-perfect-square/
+ */
+var isPerfectSquare = function (num) {
+  if (num < 2) return true;
+
+  let left = 2;
+  let right = Math.floor(num / 2);
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    const square = mid * mid;
+
+    if (square === num) return true;
+
+    square < num ? (left = mid + 1) : (right = mid - 1);
+  }
+
+  return false;
+};
