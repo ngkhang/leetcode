7b6f4ba0aa61240be0cd3de95490b943f753e32b
Khang Nguyen <ngkhang0220@gmail.com>
2026-03-16 13:53:52 +0700

    add: 88. Merge Sorted Array

---
+++ solutions/ts/88-merge-sorted-array.ts
@@ -0,0 +1,22 @@
+/**
+ * @link https://leetcode.com/problems/merge-sorted-array/
+ */
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+export function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let k = m + n - 1;
+    let j = n - 1;
+    let i = m - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+}
